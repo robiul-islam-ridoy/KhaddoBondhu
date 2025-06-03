3d699dc555bee119f423d04bb7e109e193184828
emptymahbob <emptymahbob@gmail.com>
2025-06-04 00:53:04 +0600

    Made home page, menubar, and create post form.

---
+++ app/src/main/java/com/example/khaddobondhu/ui/profile/ProfileFragment.java
@@ -0,0 +1,25 @@
+package com.example.khaddobondhu.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.example.khaddobondhu.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+} 
